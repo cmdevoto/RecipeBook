f71f8b4a3ebb40b4238d56cd4b8d98d75b342e21
Stephanie Keene <skeene@nd.edu>
2021-03-13 15:16:29 -0800

    stateResult still undefined

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ import {
 import { Header } from "./components/Header.js";
 import { Recipe } from "./components/Recipe.js";
 
-//const axios = window.axios;
-
-
+/*
 function getRecipes(){
+  const axios = window.axios;
   axios.get('src/recipes.json')
     .then(function (response) {
       // handle success
@@ -22,17 +21,22 @@ function getRecipes(){
       // handle error
       console.log(error);
     });;
+}*/
+
+//this version of getRecipes returns a 'promise' not JSON object
+export function getRecipes() {
+  const axios = window.axios;
+  return axios.get('src/recipes.json').then((response) => {
+    return response.data.data;
+  });
 }
-
-console.log("test")
-//const axiosTestResult = getRecipes(); 
-
-// create recipe list component
+console.log("Get Recipe return")
+const testVar = console.log(getRecipes())
 
   function App() { 
     //create 2 recipe arrays and add them to the state using the useState hook
-    const [recipes, setRecipes] = useState([]);  
-
+    //const [recipes, setRecipes] = useState({name: 'test', ingredients: 'test', steps:'test', notes:'test', imgPath:'test' });  
+    const [recipes, setRecipes] = useState({})
     // the useEffect hook is used here to load user data asynchronously
     useEffect(() => {
       return getRecipes().then(function (data) {
@@ -44,17 +48,18 @@ console.log("test")
       <${Header} title="RecipeList">
         Welcome to your recipe booklet
       </${Header}>
-      <${Recipe} name="focaccia" ingredients="water" steps="stir" imgPath="../public/images/focaccia.JPG">
+      <${Recipe}>
       </${Recipe}>
       <ul>
       ${recipes.map(
         (recipe) =>
-          html` <li key="${user}">
+          html` <li key="${recipe}">
             ${recipe.name} | ${recipe.ingredients} ${recipe.steps}
           </li>`
       )}
       </ul>
     `;
+    
   }
   
   render(html` <${App} /> `, document.getElementById("app"));
